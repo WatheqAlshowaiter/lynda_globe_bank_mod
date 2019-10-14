cfe2812cac14e9d13f4cc4f0fff6a5ae6f1458f9
Watheq <watheq.alshowaiter@gmail.com>
2019-10-14 15:01:42 +0300

    done vid 39 - make a show .php and display subjects based on id

--- private/query_functions.php
+++ private/query_functions.php
@@ -32,3 +32,18 @@ function find_all_pages()
     $result->execute();
     return $result;
 }
+
+function fnd_subject_by_id($id)
+{
+    global $db;
+
+    $sql  = "SELECT * FROM subjects ";
+    $sql .= "WHERE id = '" . $id . "'"; // containing 'id' with single quotes is for seacurity 
+    $result = $db->query($sql);
+    confirm_result_set($result);
+    $result->execute();
+    $subject = $result->fetch(PDO::FETCH_ASSOC);
+    $result->closeCursor();  // closing cursers or freeing results is in the result set not array you will return 
+
+    return $subject; // returns assoc array not a result set  
+}
--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -3,15 +3,27 @@
 
 // $id =  isset($_GET['id']) ? $_GET['id']: '1'; // before php 7.0
 $id = $_GET['id'] ?? '1'; // like previous one but for > PHP 7.0  
-?>
+
+$subject = fnd_subject_by_id($id);
+
+
+ ?>
 <?php $page_title = "عرض العنوان"; ?>
 
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
-<div id="content" class="container">
+<div id="content" class="container subject-show">
     <div>
         <a class="" href="<?= url_for('/staff/subjects/index.php'); ?>">&laquo; العودة للقائمة </a>
-        <h3>معرف العنوان: <?= h($id) ?></h3>
+        <h1>العنوان: <?= h($subject['menu_name']) ?></h1>
+
+        <div class="subject-attributes">
+        <ul class=""> 
+            <li><span> اسم العنوان</span> <?php echo h($subject['menu_name']); ?></li>
+            <li><span> الموقع</span> <?php echo h($subject['position']); ?></li>
+            <li><span>  الظهور</span> <?php echo $subject['visible'] == '1' ? 'true' : 'false'; ?></li> 
+        </ul>
+        </div>
 
     </div>
 
